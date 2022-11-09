0e2c2c4f3b714171c7798d63fb4de61b2696a3e2
Iraj <rustamahang@gmail.com>
2022-11-09 13:07:31 +0000

    add user Routers

---
+++ src/post/postRouters.js
@@ -0,0 +1,11 @@
+const {Router}=require("express")
+const {createPost, readPost, updatePost, deletePost}=require("./postControllers")
+
+const postRouter = Router()
+
+postRouter.get("/readPost", readPost);
+postRouter.post("/createPost", createPost);
+postRouter.put("/updatePost", updatePost);
+postRouter.delete("/deletePost", deletePost);
+
+module.exports = postRouter
