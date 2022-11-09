f446405a9567d729791dfc81f53d33e8c0181295
jdaniels98 <jackdanielswork98@gmail.com>
2022-11-09 15:39:57 +0000

    add userM

--- src/user/userModel.js
+++ src/user/userModel.js
@@ -72,6 +72,6 @@ userSchema.statics.findByCredentials = async (username, password) => {
     }
 }
 
-const User = mongoose.model("user", userSchema)
+const User = mongoose.model("User", userSchema)
 
 module.exports = User
