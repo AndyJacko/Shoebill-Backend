3dc8515a7e16fa528257968962d5386574af768a
jdaniels98 <jackdanielswork98@gmail.com>
2022-11-09 12:04:43 +0000

    add realname

--- src/user/userModel.js
+++ src/user/userModel.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    realname: {
+        type: String,
+        required: true
+    },
     joindate: {
         type: Date,
         default: Date.now,
