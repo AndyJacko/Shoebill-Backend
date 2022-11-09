ca76e012612f99aadea16994890d208e8087a1c4
jdaniels98 <jackdanielswork98@gmail.com>
2022-11-09 15:37:39 +0000

    add posts to UM

--- src/user/userModel.js
+++ src/user/userModel.js
@@ -57,6 +57,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         link: true
     },
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    }]
 })
 
 userSchema.statics.findByCredentials = async (username, password) => {
